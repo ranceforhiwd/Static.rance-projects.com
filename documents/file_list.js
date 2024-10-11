5e95e3017d4d82d9c4dbb91c8cd65a78bcaaaecc
Rance Aaron <ranceaaron941@gmail.com>
2024-10-11 17:37:54 +0000

    created function for globe

--- documents/file_list.js
+++ documents/file_list.js
@@ -1,53 +1,56 @@
 //test append to page before ajax call
 $("#upload-list #entries").append('<p>Hi World</p>');
 var API_URL = 'https://du2c8lhymg.execute-api.us-east-1.amazonaws.com/getObject';  
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNjViMzZhNS00MGJlLTQ4YTktOWUzNS1mYjNkZjNkYjViMmYiLCJpZCI6MzQyMjcsImlhdCI6MTYwMDAzNTkxMX0.EQBtegHNt-HolrydRiNV0gD75tu0cbpo57K-Hwfcu4E';
               
-    $.ajax({
-          url: API_URL,
-          type: 'GET',
-          dataType: 'json',
-          headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'
+  $.ajax({
+        url: API_URL,
+        type: 'GET',
+        dataType: 'json',
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'
 },
-          success: function(data){                              
-              fileStr = JSON.stringify(data);
-              fileArr = fileStr.split(',');
+        success: function(data){                              
+            fileStr = JSON.stringify(data);
+            fileArr = fileStr.split(',');
 
-              $('#upload-list #entries').append('<table class="table"></table>');
-              $('table').append('<thead />');
-              $('thead').append('<tr />');
-              $('thead tr').append('<th scope="col">Row</th>');
-              $('thead tr').append('<th scope="col">Filename</th>');
-              $('thead tr').append('<th scope="col">Date</th>');
-              $('table').append('<tbody />');
-                            
-              for(x in fileArr){
-                tablerow = '<tr>' +
-                      '<th scope="row"><input class="form-check-input" type="radio" name="gridRadios" id="gridRadios" value="option"></th>' +
-                      '<td>'+fileArr[x]+'</td>' +
-                      '<td>2016-05-25</td>' +
-                      '</tr>';
-                    $('tbody').append(tablerow);
-              }
-              
-          }
-    });
+            $('#upload-list #entries').append('<table class="table"></table>');
+            $('table').append('<thead />');
+            $('thead').append('<tr />');
+            $('thead tr').append('<th scope="col">Row</th>');
+            $('thead tr').append('<th scope="col">Filename</th>');
+            $('thead tr').append('<th scope="col">Date</th>');
+            $('table').append('<tbody />');
+                          
+            for(x in fileArr){
+              tablerow = '<tr>' +
+                    '<th scope="row"><input class="form-check-input" type="radio" name="gridRadios" id="gridRadios" value="option"></th>' +
+                    '<td>'+fileArr[x]+'</td>' +
+                    '<td>2016-05-25</td>' +
+                    '</tr>';
+                  $('tbody').append(tablerow);
+            }
+            
+        }
+  });
     
 
-    // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-    const viewer = new Cesium.Viewer('cesiumContainer', {
-      terrain: Cesium.Terrain.fromWorldTerrain(),
-    });    
+    function globe(){
+      Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNjViMzZhNS00MGJlLTQ4YTktOWUzNS1mYjNkZjNkYjViMmYiLCJpZCI6MzQyMjcsImlhdCI6MTYwMDAzNTkxMX0.EQBtegHNt-HolrydRiNV0gD75tu0cbpo57K-Hwfcu4E';
 
-    // Fly the camera to San Francisco at the given longitude, latitude, and height.
-    viewer.camera.flyTo({
-      destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 11400),
-      orientation: {
-        heading: Cesium.Math.toRadians(0.0),
-      },
-      duration: 20
-    });
+      // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+      const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrain: Cesium.Terrain.fromWorldTerrain(),
+      });    
 
-    // Add Cesium OSM Buildings, a global 3D buildings layer.
-    const buildingTileset = await Cesium.createOsmBuildingsAsync();
-    viewer.scene.primitives.add(buildingTileset);
+      // Fly the camera to San Francisco at the given longitude, latitude, and height.
+      viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 11400),
+        orientation: {
+          heading: Cesium.Math.toRadians(0.0),
+        },
+        duration: 20
+      });
+
+      // Add Cesium OSM Buildings, a global 3D buildings layer.
+      const buildingTileset = await Cesium.createOsmBuildingsAsync();
+      viewer.scene.primitives.add(buildingTileset);
+    }
