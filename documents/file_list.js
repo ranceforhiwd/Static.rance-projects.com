2bceebf776e351eae356119e17efbac41909e109
Rance Aaron <ranceaaron941@gmail.com>
2024-10-11 19:01:37 +0000

    reverted last changes

--- documents/file_list.js
+++ documents/file_list.js
@@ -31,26 +31,3 @@ var API_URL = 'https://du2c8lhymg.execute-api.us-east-1.amazonaws.com/getObject'
             
         }
   });
-    
-
-    function globe(){
-      Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNjViMzZhNS00MGJlLTQ4YTktOWUzNS1mYjNkZjNkYjViMmYiLCJpZCI6MzQyMjcsImlhdCI6MTYwMDAzNTkxMX0.EQBtegHNt-HolrydRiNV0gD75tu0cbpo57K-Hwfcu4E';
-
-      // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-      const viewer = new Cesium.Viewer('cesiumContainer', {
-        terrain: Cesium.Terrain.fromWorldTerrain(),
-      });    
-
-      // Fly the camera to San Francisco at the given longitude, latitude, and height.
-      viewer.camera.flyTo({
-        destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 11400),
-        orientation: {
-          heading: Cesium.Math.toRadians(0.0),
-        },
-        duration: 20
-      });
-
-      // Add Cesium OSM Buildings, a global 3D buildings layer.
-      const buildingTileset = await Cesium.createOsmBuildingsAsync();
-      viewer.scene.primitives.add(buildingTileset);
-    }
