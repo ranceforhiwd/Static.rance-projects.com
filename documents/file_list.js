ec7bf7f849a47aef4a581b02e1c7e65a8dd5b829
Rance Aaron <ranceaaron941@gmail.com>
2024-10-11 12:06:46 +0000

    test.     files

---
+++ documents/file_list.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    var API_URL = 'https://du2c8lhymg.execute-api.us-east-1.amazonaws.com/getObject';  
+              
+    $.ajax({
+          url: API_URL,
+          type: 'GET',
+          dataType: 'json',
+          headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'
+},
+          success: function(data){                              
+              fileStr = JSON.stringify(data);
+              fileArr = fileStr.split(',');
+
+              $('#entries').append('<table class="table"></table>');
+              $('table').append('<thead />');
+              $('thead').append('<tr />');
+              $('thead tr').append('<th scope="col">Row</th>');
+              $('thead tr').append('<th scope="col">Filename</th>');
+              $('thead tr').append('<th scope="col">Date</th>');
+              $('table').append('<tbody />');
+              
+              tablerow = '<tr>' +
+                      '<th scope="row"><input class="form-check-input" type="radio" name="gridRadios" id="gridRadios" value="option"></th>' +
+                      '<td>filename.txt</td>' +
+                      '<td>2016-05-25</td>' +
+                    '</tr>';
+              $('tbody').append(tablerow);
+              
+              for(x in fileArr){
+                $('#entries').append('<p>'+x+':'+fileArr[x]+'</p>');
+              }
+              
+          }
+    });   
+  });
