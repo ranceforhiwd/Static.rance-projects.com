8ab4d095bb763ba67f86fcaa5cc502ddca71fa0a
Rance Aaron <ranceaaron941@gmail.com>
2024-10-11 20:41:34 +0000

    updated second coordinates

--- documents/file_list.js
+++ documents/file_list.js
@@ -31,3 +31,33 @@ var API_URL = 'https://du2c8lhymg.execute-api.us-east-1.amazonaws.com/getObject'
             
         }
   });
+
+  $.ajax({
+      url: API_URL,
+      type: 'GET',
+      dataType: 'json',
+      headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'
+},
+      success: function(data){                              
+          adxStr = JSON.stringify(data);
+          adxArr = adxStr.split(',');
+
+          $('#globe #addresses').append('<table class="table"></table>');
+          $('table').append('<thead />');
+          $('thead').append('<tr />');
+          $('thead tr').append('<th scope="col">Row</th>');
+          $('thead tr').append('<th scope="col">Filename</th>');
+          $('thead tr').append('<th scope="col">Date</th>');
+          $('table').append('<tbody />');
+                        
+          for(x in adxArr){
+            tablerow = '<tr>' +
+                  '<th scope="row"><input class="form-check-input" type="radio" name="gridRadios" id="gridRadios" value="option"></th>' +
+                  '<td>'+adxArr[x]+'</td>' +
+                  '<td>2016-05-25</td>' +
+                  '</tr>';
+                $('tbody').append(tablerow);
+          }
+          
+      }
+});
