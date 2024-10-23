ff3f7515169bd1f7ece90264be10b72eb71102fa
Rance Aaron <ranceaaron941@gmail.com>
2024-10-23 14:41:07 +0000

    updating globe

---
+++ globe/globe.js
@@ -0,0 +1,28 @@
+// The URL on your server where CesiumJS's static files are hosted.
+window.CESIUM_BASE_URL = '/';
+
+import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+// Your access token can be found at: https://ion.cesium.com/tokens.
+// Replace `your_access_token` with your Cesium ion access token.
+
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNjViMzZhNS00MGJlLTQ4YTktOWUzNS1mYjNkZjNkYjViMmYiLCJpZCI6MzQyMjcsImlhdCI6MTYwMDAzNTkxMX0.EQBtegHNt-HolrydRiNV0gD75tu0cbpo57K-Hwfcu4E';
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Viewer('cesiumContainer', {
+  terrain: Terrain.fromWorldTerrain(),
+});    
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation: {
+    heading: CesiumMath.toRadians(0.0),
+    pitch: CesiumMath.toRadians(-15.0),
+  }
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = await createOsmBuildingsAsync();
+viewer.scene.primitives.add(buildingTileset);   
