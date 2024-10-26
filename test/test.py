b58a79ae25048018c66166408bcc1b97a099fb9f
Rance Aaron <ranceaaron941@gmail.com>
2024-10-26 11:22:42 +0000

    flynow bugfixes

---
+++ test/test.py
@@ -0,0 +1,24 @@
+import json
+
+t = [
+	{
+'json_data': [
+['8197', Decimal('59'), Decimal('1056'), Decimal('92'), Decimal('1067'), Decimal('16')], 
+['Happygo', Decimal('98'), Decimal('1056'), Decimal('161'), Decimal('1070'), Decimal('16')], 
+['Lucky', Decimal('167'), Decimal('1056'), Decimal('209'), Decimal('1070'), Decimal('16')], 
+['Lane', Decimal('215'), Decimal('1056'), Decimal('249'), Decimal('1067'), Decimal('16')], 
+['Page', Decimal('683'), Decimal('1056'), Decimal('716'), Decimal('1070'), Decimal('16')], 
+['5', Decimal('722'), Decimal('1056'), Decimal('729'), Decimal('1067'), Decimal('16')], 
+['of', Decimal('735'), Decimal('1056'), Decimal('747'), Decimal('1067'), Decimal('16')], 
+['49', Decimal('751'), Decimal('1056'), Decimal('767'), Decimal('1067'), Decimal('16')]
+], 
+'png_file_name': './Testpdf10.pdf~05.png', 
+'pdf_file_name': 'Testpdf10.pdf', 
+'pngname_sk': 'pngname_pk-./Testpdf10.pdf~05.png', 
+'pngname_pk': 'pngname_pk'
+}
+]
+
+x = t.json_data
+y = x[0]
+print(x)
